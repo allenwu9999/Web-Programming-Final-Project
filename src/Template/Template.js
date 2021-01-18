7e6e3a78e3e9d9cc60f6d4b4c67fcfe649a8b9ed
MartianSheep <mkmarsscience@gmail.com>
2021-01-18 13:23:52 +0800

    fix scroll flow

--- src/Template/Template.js
+++ src/Template/Template.js
@@ -2,66 +2,70 @@ import "antd/dist/antd.css";
 import React from "react";
 
 import { Layout } from "antd";
+import { BackTop } from 'antd';
 
 import Navigation from "./Navigation";
 
 const { Header, Content, Footer } = Layout;
 
 function Template(props) {
-  // should get from user db
-  const Notifications = [];
+	// should get from user db
+	const Notifications = [];
 
-  return (
-    <Layout>
-      <Header
-        style={{
-          position: "fixed",
-          width: "100%",
-          backgroundColor: "#FFFFFF",
-          height: 64,
-        }}
-      >
-        {
-          <Navigation
-            loggined={false}
-            reviewer={true}
-            notifications={Notifications}
-          />
-        }
-      </Header>
+	return (
+		<Layout>
+			<Header
+				style={{
+					position: "fixed",
+					width: "100%",
+					backgroundColor: "#FFFFFF",
+					height: 64,
+					zIndex: 1
+				}}
+			>
+				{
+					<Navigation
+						loggined={false}
+						reviewer={true}
+						notifications={Notifications}
+					/>
+				}
+			</Header>
 
-      <Content
-        style={{
-          padding: "0 50px",
-          marginTop: 64,
-          background: "#E0E0E0",
-        }}
-      >
-        <div style={{ padding: 24, minHeight: window.innerHeight - 64 }}>
-          {props.content}
-        </div>
-      </Content>
+			<Content
+				style={{
+					padding: "0 50px",
+					marginTop: 64,
+					background: "#E0E0E0",
+				}}
+			>
+				<div style={{ padding: 24, minHeight: (window.innerHeight - 64) }}>
+					{props.content}
+				</div>
+			</Content>
 
-      <Footer
-        style={{
-          textAlign: "center",
-          backgroundColor: "#000000",
-          color: "#FFFFFF",
-        }}
-      >
-        Ideas Republica{" "}
-        <span
-          style={{
-            display: "inline-block",
-            transform: "rotateY(180deg)",
-          }}
-        >
-          &copy;
-        </span>{" "}
-        2020
-      </Footer>
-    </Layout>
-  );
+			<Footer
+				style={{
+					textAlign: "center",
+					backgroundColor: "#000000",
+					color: "#FFFFFF",
+				}}
+			>
+				Ideas Republica{" "}
+				<span
+					style={{
+						display: "inline-block",
+						transform: "rotateY(180deg)",
+					}}
+				>
+					&copy;
+				</span>{" "}
+				2020-2021
+			</Footer>
+
+			<BackTop duration={450} visibilityHeight={200} />
+		</Layout>
+	);
 }
 
 export default Template;
